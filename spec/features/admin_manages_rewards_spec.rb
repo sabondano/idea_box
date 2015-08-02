975f4db23a061ac0143f4f3bf97874ddb8c799c4
Sebastian Abondano <sabondano1@gmail.com>
2015-08-02 13:17:58 -0600

    Implement only admins see 'add reward' link

--- spec/features/admin_manages_rewards_spec.rb
+++ spec/features/admin_manages_rewards_spec.rb
@@ -15,4 +15,14 @@ feature "admin manages rewards" do
 
     expect(page).to have_content("iPod")
   end
+
+  scenario "regular users cant add rewards" do
+    user = User.create(username: "sebastian",
+                       password: "asdf")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit rewards_path
+
+    expect(page).to_not have_link("Add Reward")
+  end
 end
